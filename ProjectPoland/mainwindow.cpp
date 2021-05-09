ed2cf52dfbbc9cc567d04d1b33e7c34b118bb80e
Alexander <63105046+sanyok2611@users.noreply.github.com>
2021-05-09 22:01:22 +0300

    Upgrade UI

--- ProjectPoland/mainwindow.cpp
+++ ProjectPoland/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -19,9 +20,18 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     QStringList fileName = QFileDialog::getOpenFileNames(this,"Choose files to read", "C://");
-    QMessageBox::information(this,"...", fileName[0]);
+    //QMessageBox::information(this,"...", fileName[0]);
     foreach(QString str, fileName)
     {
-        ui->listWidget->addItem(str);
+        QListWidgetItem *item = new QListWidgetItem(QIcon(":/rec/ImageIcon.png"),str);
+        ui->listWidget->addItem(item);
     }
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    int w = ui->label->width();
+    int h = ui->label->height();
+    QPixmap pix(ui->listWidget->currentItem()->text());
+    ui->label->setPixmap(pix.scaled(w,h, Qt::KeepAspectRatio));
 }
